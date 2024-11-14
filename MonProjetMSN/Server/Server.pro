062db5634153d8e456aebb8e452075ea7083ae11
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-11-14 15:58:55 +0100

    <add><intialize project with QTcreator with an empty parent project with two sub-project for the client and the server

---
+++ MonProjetMSN/Server/Server.pro
@@ -0,0 +1,6 @@
+QT += core network
+TEMPLATE = app
+TARGET = Serveur
+SOURCES += main.cpp \
+           serveur.cpp
+HEADERS += serveur.h
---
+++ MonProjetMSN/Server/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_Server_FILE C:/Users/tslem/Desktop/Laplateforme/bachelore2/depot/Cpp/projetGroupe/CPPRT/MonProjetMSN/Server/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/Server.exe)
+set(__QT_DEPLOY_TARGET_Server_RUNTIME_DLLS C:/Qt/6.7.1/mingw_64/bin/Qt6Core.dll)
