66a1b7347ae7241e90920aa6ee9a1c2f052f51c0
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-11-14 16:00:45 +0100

    <add><intialize project with QTcreator with an empty parent project with two sub-project for the client and the server

---
+++ MonProjetMSN/Client/Client.pro
@@ -0,0 +1,7 @@
+QT += core gui network
+TEMPLATE = app
+TARGET = Client
+SOURCES += main.cpp \
+           mainwindow.cpp
+HEADERS += mainwindow.h
+FORMS += mainwindow.ui
---
+++ MonProjetMSN/Client/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_Client_FILE C:/Users/tslem/Desktop/Laplateforme/bachelore2/depot/Cpp/projetGroupe/CPPRT/MonProjetMSN/Client/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/Client.exe)
+set(__QT_DEPLOY_TARGET_Client_RUNTIME_DLLS C:/Qt/6.7.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Core.dll)
