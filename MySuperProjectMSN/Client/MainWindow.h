ef03ba41657ec0674ec3c995b69935ce6739c34e
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-11-22 14:52:00 +0100

    <feat>[mainWindow] Add error handling and reconnect functionality in MainWindow

--- MySuperProjectMSN/Client/MainWindow.h
+++ MySuperProjectMSN/Client/MainWindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 #include <QTcpSocket>
+#include <QTimer>
+#include <QLabel>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -25,10 +27,17 @@ private slots:
     void onSendButtonClicked();
     void onReadyRead();
     void onDisconnected();
+    void onErrorOccurred(QAbstractSocket::SocketError socketError);
+    void attemptReconnect();
+
 
 private:
     Ui::MainWindow *ui;
     QTcpSocket *socket;
+    QTimer *reconnectTimer;
+    int reconnectInterval =6000; // this is ms
+    QLabel *statusLabel;
+    void setupStatusBar();
 };
 
 #endif // MAINWINDOW_H
--- MySuperProjectMSN/Client/mainwindow.cpp
+++ MySuperProjectMSN/Client/mainwindow.cpp
@@ -1,5 +1,6 @@
-#include <QTcpSocket>
 #include <QCloseEvent>
+#include <QDebug>
+#include <QMessageBox>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -15,6 +16,8 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(ui->sendButton, &QPushButton::clicked, this, &MainWindow::onSendButtonClicked);
     connect(socket, &QTcpSocket::readyRead, this, &MainWindow::onReadyRead);
     connect(socket, &QTcpSocket::disconnected, this, &MainWindow::onDisconnected);
+    connect(socket, QOverload<QAbstractSocket::SocketError>::of(&QTcpSocket::errorOccurred), this, &MainWindow::onErrorOccurred);
+    setupStatusBar();
 }
 
 MainWindow::~MainWindow()
@@ -41,6 +44,28 @@ void MainWindow::onDisconnected() {
     qDebug() << "Socket disconnected";
 }
 
+void MainWindow::onErrorOccurred(QAbstractSocket::SocketError socketError) {
+    qDebug() << "Socket error:" << socketError << socket->errorString();
+    ui->statusbar->showMessage("Error: " + socket->errorString());
+
+    if (socketError == QAbstractSocket::ConnectionRefusedError ||
+        socketError == QAbstractSocket::RemoteHostClosedError ||
+        socketError == QAbstractSocket::NetworkError) {
+        reconnectTimer->start(reconnectInterval);
+    }
+}
+
+void MainWindow::attemptReconnect() {
+    qDebug() << "Attempting to reconnect to the server...";
+    ui->statusbar->showMessage("Reconnecting...");
+    socket->abort();
+    socket->connectToHost("127.0.0.1", 1234);
+    if (socket->state() == QAbstractSocket::ConnectedState) {
+        reconnectTimer->stop();
+        ui->statusbar->showMessage("Reconnected.");
+    }
+}
+
 void MainWindow::onSendButtonClicked() {
     if (socket->state() == QAbstractSocket::ConnectedState) {
         socket->write(ui->messageLineEdit->text().toUtf8());
@@ -53,3 +78,19 @@ void MainWindow::onReadyRead() {
     QByteArray data = socket->readAll();
     ui->chatTextEdit->append(QString::fromUtf8(data));
 }
+
+void MainWindow::setupStatusBar()
+{
+    // Create a QLabel for status messages
+    statusLabel = new QLabel("Connecting...", this);
+
+    // Add widgets to the status bar
+    ui->statusbar->addWidget(statusLabel);        // Left-aligned widget
+
+    // Example: Update status when connected
+    connect(socket, &QTcpSocket::connected, this, [this]() {
+        statusLabel->setText("Connected");
+    });
+}
+
+
